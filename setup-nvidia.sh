d21fa5d214f04de099269f1a3ec8cbe0e8fe7e7c
Dezsoe Babai <de.babai2000@gmail.com>
2025-05-24 20:55:11 +0200

    add Nvidia wrapper
    Signed-off-by: Dezsoe Babai <de.babai2000@gmail.com>

--- setup-nvidia.sh
+++ setup-nvidia.sh
@@ -2,6 +2,7 @@
 
 nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
 nix-env -iA nixgl.auto.nixGLDefault  
+nix-env -iA nixgl.auto.nixGLNvidia
 
 exname=$(ls ~/.nix-profile/bin/ | grep nixGLNvidia)
 echo "To be able to use the GPU in the Nix environment, use following command when entering it: ${exname} nix-shell"
